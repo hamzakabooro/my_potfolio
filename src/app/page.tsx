8ca93ecbe2672303f5470db4fbf623ab4045970c
hamzakabooro <kaboorohamzakhan32>
2024-12-26 01:59:48 +0500

    -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import Hero from "@/components/Hero";
-import Image from "next/image";
-import Navbar from "@/components/Navbar"
 
 
 export default function Home() {
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import Link from 'next/link'
-import Navbar from './Navbar'
+
 import Image from 'next/image'
 
 const Hero = () => {
