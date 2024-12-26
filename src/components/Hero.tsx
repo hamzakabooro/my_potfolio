6ab2d89170e4b712c4568a7af5127e111ca9adb1
hamzakabooro <kaboorohamzakhan32>
2024-12-27 00:13:59 +0500

    -

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,10 +6,10 @@ const Hero = () => {
   return (
     <div>
       <div className=' container grid lg:grid-cols-2  md:grid-cols-1 mt-20]'>
-        <Image src={"/banner.jfif"} alt='' width={300} height={100} className="m-28 rounded-full" />
+        <Image src={"/profile.jpg"} alt='profile pic' width={200} height={100} className="m-28 ml-10  h-[300px] w-[280px] border border-red-600 rounded-full" />
         <div className='text-[100px] sm:text-[80px] font-bold leading-tight flex justify-center items-center'>
 
-          <div>
+          <div className='mr-'>
             <p>I'm</p>
             <p>Hamza</p>
             <p>khan</p>
