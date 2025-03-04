74d636f802ba792bf9e9889327f844746cc78167
hamzakabooro <kaboorohamzakhan32>
2025-03-04 16:59:56 +0500

    -

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <Image src={"/hamza.png.jpg"} alt='profile pic' width={200} height={100} className="m-28 ml-10  h-[400px] w-[380px] border border-red-700 rounded-full" />
         <div className='text-[100px] sm:text-[80px] font-bold leading-tight flex justify-center items-center'>
 
-          <div className='mr-'>
+          <div>
             <p>I'm</p>
             <p>Hamza</p>
             <p>khan</p>
