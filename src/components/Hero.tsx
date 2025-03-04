fdb02b1cd2e3c750150ab27473d43dc2d0cd5284
hamzakabooro <kaboorohamzakhan32>
2025-03-04 17:09:27 +0500

    -

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react"
 
-import Image from 'next/image'
+import Image from "next/image"
 
 const Hero = () => {
   return (
     <div>
-      <div className=' container grid lg:grid-cols-2  md:grid-cols-1 mt-20]'>
-        <Image src={"/hamza.png.jpg"} alt='profile pic' width={200} height={100} className="m-28 ml-10  h-[400px] w-[380px] border border-red-700 rounded-full" />
+      <div className="container grid lg:grid-cols-2  md:grid-cols-1 mt-20]">
+        <Image src={"/hamza.png.jpg"} alt="profile pic" width={200} height={100} className="m-28 ml-10  h-[400px] w-[380px] border border-red-700 rounded-full" />
         <div className='text-[100px] sm:text-[80px] font-bold leading-tight flex justify-center items-center'>
 
           <div>
-            <p>Im</p>
+            <p>I m</p>
             <p>Hamza</p>
             <p>khan</p>
           </div>
@@ -21,6 +21,6 @@ const Hero = () => {
 
 
   );
-};
+}
 
 export default Hero
