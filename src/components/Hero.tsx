09f583740e3fe91db5306bf710cc27d6b337cc8a
hamzakabooro <kaboorohamzakhan32>
2025-03-04 16:53:13 +0500

    -

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div>
       <div className=' container grid lg:grid-cols-2  md:grid-cols-1 mt-20]'>
-        <Image src={"/profile.jpg"} alt='profile pic' width={200} height={100} className="m-28 ml-10  h-[300px] w-[280px] border border-red-600 rounded-full" />
+        <Image src={"/hamza.png.jpg"} alt='profile pic' width={200} height={100} className="m-28 ml-10  h-[400px] w-[380px] border border-red-700 rounded-full" />
         <div className='text-[100px] sm:text-[80px] font-bold leading-tight flex justify-center items-center'>
 
           <div className='mr-'>
